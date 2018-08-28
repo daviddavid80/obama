80b22cfb916fbcdda00eba13b4dc8950d1b684d4
daviddavid80 <42768599+daviddavid80@users.noreply.github.com>
2018-08-28 10:58:12 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,26 @@
+alert("Welcome to the the Site About Barrack Obama!. Click ok to Enter")
+
+$(document).ready(function(){
+$("#btn1").click(function(){
+$("#area1").toggle();
+
+});   
+
+
+$("#btn2").click(function(){
+$("#area2").toggle();
+
+}); 
+
+$("#btn3").click(function(){
+$("#area3").toggle();
+
+}); 
+
+$("#btn4").click(function(){
+$("#area4").toggle();
+
+}); 
+
+
+});
